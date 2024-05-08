a786bc9ec939dc35b6729c59b9dc87dedd8fdb8c
Ntlele <ntleledavid07@gmail.com>
2024-05-08 06:43:43 +0000

    concatenating edges of a string

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str[39:67] + str[-22:-17] + str[0:6])
+print(str[39:66] + ' ' + str[-22:-18] + ' ' + str[:6])
