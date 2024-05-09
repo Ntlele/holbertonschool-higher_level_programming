f73424e4e72076aa33fd2792433344d46a679e2e
Ntlele <ntleledavid07@gmail.com>
2024-05-09 14:10:29 +0000

    Print all posible combinations of two numbers

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/src/bin/python3
+for x in range(0, 10):
+    for y in range(0, 10):
+        if x != y and x < y:
+            if (str(x) + str(y)) != "89":
+                print("{}{}".format(x, y),end=", ")
+print("89")
+
