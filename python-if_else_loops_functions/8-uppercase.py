30c50d3e97aec661317b19ad1bae15af32937603
Ntlele <ntleledavid07@gmail.com>
2024-05-09 16:29:18 +0000

    print string to upper case

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    up_str = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            up_char = chr(ord(char) - 32)
+            up_str += up_char
+        else:
+            up_str += char
+    print(up_str)
+    return up_str
