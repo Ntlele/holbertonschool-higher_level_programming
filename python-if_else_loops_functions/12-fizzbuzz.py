c748f10e9f6c9581741f80318619ea819023cfad
Ntlele <ntleledavid07@gmail.com>
2024-05-10 06:27:55 +0000

    Numbers form 1 to 100

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 for i in range(1, 101):
     if (i % 5) == 0 and (i % 3) == 0:
         print("FizzBuzz", end=" ")
-    if (i % 5) == 0:
+    elif (i % 5) == 0:
         print("Buzz", end=" ")
     elif (i % 3) == 0:
         print("Fizz", end=" ")
