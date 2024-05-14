dac92f72efdcb4f3ebe7e4e68d8074caedc17f90
Ntlele <ntleledavid07@gmail.com>
2024-05-14 18:51:50 +0000

    Python data structures

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return my_list
     else:
         new_list = my_list[:]
