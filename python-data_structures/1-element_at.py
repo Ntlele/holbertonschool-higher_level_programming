df50dc15ce53b01e3a00539beb7f042000a2b6d9
Ntlele <ntleledavid07@gmail.com>
2024-05-14 18:19:40 +0000

    Python data structures

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):   
+    if idx < 0:
+        return "none"
+
+    elif idx >= len(my_list):
+        "None"
+    else:
+        return my_list[idx]
