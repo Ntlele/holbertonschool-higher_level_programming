a1294466771bbb9bdb09abeadc9985b1dcc6f99c
Ntlele <ntleledavid07@gmail.com>
2024-05-19 11:58:28 +0000

    Python exceptions

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+1-square.py: contains the Square class which defines a square
+"""
+
+
+class Square:
+    """
+    A class that is used to represent a Square
+
+    Atributes:
+        attr1 (size): Sizesize of the side of the square
+    """
+
+    def __init__(self, size=0):
+        """Initializer _Square__size"""
+        self.__size = size
