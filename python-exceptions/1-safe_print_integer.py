e5d646b38848b3d21e2bb7f73be67e1050dab002
Ntlele <ntleledavid07@gmail.com>
2024-05-19 08:16:07 +0000

    Python exceptions

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
+    except (ValueError, TypeError):
         return False
-    return True
+    else:
+        return True
