79d6adc455418b83061f5617cbe1f57a6419e2ca
Ntlele <ntleledavid07@gmail.com>
2024-05-19 12:13:22 +0000

    Python exceptions

---
+++ python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+2-square.py: contains a class Square that defines a square
+"""
+
+
+class Square:
+    """
+    class Square represents a square
+    
+    Attributes:
+        attr1(_Square__size): size of the side of the square (Private)
+    """
+
+    def __init__(self, size=0):
+        """Initializer with default size = 0"""
+        if type(size) is not int:
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self._size = size
