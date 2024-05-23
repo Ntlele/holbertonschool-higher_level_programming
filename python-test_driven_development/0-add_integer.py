016417dcd12a3ecfcee8e6abc1c72c17c24dcc2f
Ntlele <ntleledavid07@gmail.com>
2024-05-23 18:14:43 +0000

    Error testing and handling

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 0-add_integers.py """
+
+def add_integer(a, b=98):
+    """ 
+    Adds two numbers
+
+    args:
+        a = first number
+        b = second number
+
+    Returns:
+        sum of two numbers
+    
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
