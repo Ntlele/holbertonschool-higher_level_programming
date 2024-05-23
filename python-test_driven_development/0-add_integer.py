24dbd9015090a6bffff13f6a806da7c7144691d6
Ntlele <ntleledavid07@gmail.com>
2024-05-23 18:18:10 +0000

    Error testing and handling

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ 0-add_integers.py """
 
+
 def add_integer(a, b=98):
-    """ 
+    """
     Adds two numbers
 
     args:
@@ -11,7 +12,7 @@ def add_integer(a, b=98):
 
     Returns:
         sum of two numbers
-    
+
     """
 
     if type(a) is not int and type(a) is not float:
