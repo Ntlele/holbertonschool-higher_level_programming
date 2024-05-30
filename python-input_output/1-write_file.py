523ccdf4bcc61a9fdc5221ca69c9fac9ca306ec9
Ntlele <ntleledavid07@gmail.com>
2024-05-30 19:44:50 +0200

    File handling in python

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,9 +7,7 @@ and returns the number of characters writen
 
 def write_file(file_name="", text=""):
     """ This function writes to a file and counts characters """
-    # Open the file in write mode and write the text to it
     with open(file_name, 'w') as file:
         file.write(text)
 
-    # Return the number of characters written
     return len(text)
