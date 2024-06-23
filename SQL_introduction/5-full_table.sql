0cc49ffecae7d051bac6178af3889471f0ee8b3d
Ntlele <ntleledavid07@gmail.com>
2024-06-23 11:20:06 +0000

    Listing all mysql databases

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Print table description for first_table in the specified database
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE `first_table`;
--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,3 +1,3 @@
 -- List all rows from the first_table in the specified database
-SELECT * FROM hbtn_0c_0.first_table;
+SELECT * FROM `first_table;
 
