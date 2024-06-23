266d62bbeb10ab12b684eef93ce68d7b2c9eea33
Ntlele <ntleledavid07@gmail.com>
2024-06-23 12:12:22 +0000

    Listing all mysql databases

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Insert a new row into the first_table in the specified database
+INSERT INTO hbtn_0c_0.first_table (id, name)
+VALUES (89, 'Best School');
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Count records where id = 89 in the first_table of the specified database
+SELECT COUNT(*)
+FROM `first_table`
+WHERE id = 89;
