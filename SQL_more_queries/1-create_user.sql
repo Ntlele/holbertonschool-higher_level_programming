173e37de97809aaf3112e2b84445b4f15fa6a314
Ntlele <ntleledavid07@gmail.com>
2024-06-23 14:37:43 +0000

    Listing all mysql databases

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,12 +1,10 @@
 -- Check if the user exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+SET @user_exists = (SELECT COUNT(*) FROM mysql.user WHERE user = 'user_0d_1');
 
--- Set the password for the user
-ALTER USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
-
--- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
-
--- Apply the changes
-FLUSH PRIVILEGES;
+-- If the user does not exist, create the user and grant privileges
+IF @user_exists = 0 THEN
+    CREATE USER 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%' WITH GRANT OPTION;
+    FLUSH PRIVILEGES;
+END IF;
 
