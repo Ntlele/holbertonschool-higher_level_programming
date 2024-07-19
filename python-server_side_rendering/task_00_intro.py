61ed1fbca389b1469e97e7b0a898629e5a3665c4
Ntlele <ntleledavid07@gmail.com>
2024-07-19 17:52:45 +0200

    Fixed the error handling

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -4,9 +4,15 @@ import os
 def generate_invitations(template, attendees):
     """ This funtion prints creates a template of invitations addressed to various people"""
 
-    if isinstance(template, str) == True and isinstance(attendees, dict) != True:
-        print("\n Data type ERROR: \n Check if the template is a string and the attendies is a dictionary\n")
+    # 
+    
+    if not isinstance(template, str):
+        print("Error: Template is not a string.")
         return
+    if not isinstance(attendees, list) or not all(isinstance(attendee, dict) for attendee in attendees):
+        print("Error: Atendees is not a list of dictionaries")
+        return
+    
     
     if not template.strip():
         print("The template should not be empty")
@@ -20,9 +26,9 @@ def generate_invitations(template, attendees):
     for i, attendee in enumerate(attendees, start=1):
         #Replacing placeholders
         invitation = template.format(
-            name=attendee.get("name", "N/A")
-            event_title=attendee.get("event_title", "N/A")
-            event_date=attendee.get("event_date", "N/A")
+            name=attendee.get("name", "N/A"),
+            event_title=attendee.get("event_title", "N/A"),
+            event_date=attendee.get("event_date", "N/A"),
             event_location=attendee.get("event_location", "N/A")
         )
 
@@ -33,4 +39,6 @@ def generate_invitations(template, attendees):
 
         print(f"Generated {t_filename}")
 
-
+# if __name__ == "main":
+#     with open('template.txt', 'r') as f:
+#         template_content = f.read()
