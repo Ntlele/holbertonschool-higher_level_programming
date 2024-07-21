af30c94765448a18f6b0e16911ac86aa090bcdf7
Ntlele <ntleledavid07@gmail.com>
2024-07-21 10:43:48 +0200

    Jinja logic

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -6,21 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 @app.route('/items')
 def items():
-    with open('items.json', 'r') as file:
-       Items = json.load(file)
-       items = Items.get("items", [])
+    with open('items.json') as f:
+        data = json.load(f)
+    items = data.get("items", [])
 
     return render_template('items.html', items=items)
 
-if __name__ == '__ main__':
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
