91cd0c892422613a713c08d11618d8dcbf4e8954
Ntlele <ntleledavid07@gmail.com>
2024-07-28 19:18:43 +0200

    DOM Manipulation with javascript

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,6 @@
+document.getElementById('add_item').addEventListener('click', function() {
+    const newItem = document.createElement('li');
+    newItem.textContent = 'Item';
+    document.querySelector('ul.my_list').appendChild(newItem);
+  });
+  
